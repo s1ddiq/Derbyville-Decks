ef858daa7c277d848acf3174e37af29d3e7fcf63
s1ddiq <siddiq.nasheed@gmail.com>
2026-04-19 18:23:05 +0200

    apply card shadow

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -12,7 +12,7 @@ function Card({
       data-slot="card"
       data-size={size}
       className={cn(
-        "group/card flex flex-col gap-6 overflow-hidden rounded-2xl bg-card py-8 text-sm text-card-foreground ring-1 ring-foreground/10 has-[>img:first-child]:pt-0 data-[size=sm]:gap-4 data-[size=sm]:py-4 *:[img:first-child]:rounded-t-xl *:[img:last-child]:rounded-b-xl border-2 border-black",
+        "group/card flex flex-col gap-6 overflow-hidden rounded-2xl bg-card py-8 text-sm text-card-foreground ring-1 ring-foreground/10 has-[>img:first-child]:pt-0 data-[size=sm]:gap-4 data-[size=sm]:py-4 *:[img:first-child]:rounded-t-xl *:[img:last-child]:rounded-b-xl border-2 border-black card-shadow",
         className,
       )}
       {...props}
